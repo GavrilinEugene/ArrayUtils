ed5fb9159a505e0595da3578c6f8090c57e560b8
EugeneGavrilin <julentver1@gmail.com>
2016-11-05 17:27:14 +0300

    ArrayUtils: methods declarations

---
+++ src/ArrayUtils.java
@@ -0,0 +1,28 @@
+/*
+утилита для работы с массивами типа int
+ */
+
+public class ArrayUtils {
+
+    public static int[] trim(int[] array, int newLength){
+        return new int[newLength];
+    }
+
+    public static int[] resize(int[] array, int newLength){
+        return new int[newLength];
+    }
+
+    public static int[] shuffle(int[] array){
+        return new int[array.length];
+    }
+
+    public static boolean compareNoRagardToOrger(int[] first, int[] second){
+        return false;
+    }
+
+    public static String print(int[] array){
+        return array.toString();
+
+    }
+
+}
